62bd1a6625451cf8a9b09f7eb7d63f74b8872529
Veronica Tomchak <vtomchak27@gmail.com>
2021-02-07 19:41:08 -0500

    idk

--- server/api/users.js
+++ server/api/users.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+
 const {User, Goal, UserGoal} = require('../db/models')
 module.exports = router
 
@@ -30,3 +31,32 @@ router.get('/:email', async (req, res, next) => {
     next(err)
   }
 })
+
+router.post('/:userId/add/:friendId', async (req, res, next)=> {
+  try {
+    const user = await User.findOne(
+      {where: {id: req.params.userId}}
+    )
+    // console.log(user.getAssociations)
+    // const friends = await User.getFriends()
+    // const friend = await User.findOne(
+    //   {where: {id: req.params.friendId}}
+    // )
+    const friends = await user.getFriends()
+    if(friends.map( friend => friend.id).includes(req.params.friendId)){
+      res.send('Already friends')
+      return;
+    } else {
+      // const friend = await User.findByPk(req.params.friendId)
+      await user.addFriend(req.params.friendId)
+      // await user.update(req.body)
+      // await user.save();
+      // await user.reload();
+      res.send('friend added')
+      return;
+    } 
+  } catch (err){
+    next(err)
+  }
+})
+
