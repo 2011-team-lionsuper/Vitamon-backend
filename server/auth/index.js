350fc8fc6f51a8f3c261ca15747fe1ea5cf525c6
Melissa Pastore <melissa.pastore@gmail.com>
2021-02-11 15:16:31 -0500

    added sanitizeHtml to log in route

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 const {User, Goal} = require('../db/models')
+const sanitizeHtml = require('sanitize-html')
 module.exports = router
 
 router.post('/login', async (req, res, next) => {
   try {
     const user = await User.findOne({
-      where: {email: req.body.email},
+      where: {email: sanitizeHtml(req.body.email)},
       include: [{model: User, as: 'friends'}, {model: Goal}]
     })
     if (!user) {
@@ -24,7 +25,7 @@ router.post('/login', async (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   try {
-    const user = await User.create(req.body)
+    const user = await User.create(s(req.body))
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
